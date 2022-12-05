0de99c7af7d83fd2e31032b26d9804ea13518283
Amal <amalprakash2025@gmail.com>
2022-12-05 23:23:56 +0530

    added home, addproducts

---
+++ src/main/java/com/nest/productappBackend/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.nest.productappBackend.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ProductController {
+
+    @PostMapping("/")
+    public String Home(){
+        return "Welcome to Home Page";
+    }
+
+    @PostMapping("/addProducts")
+    public String AddProduct(){
+        return "Add products";
+    }
+
+}
